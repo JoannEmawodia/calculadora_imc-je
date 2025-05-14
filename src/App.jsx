e0b00bdfe14266c832e5cc001bcfd22d795f65f4
JoannEmawodia <emawodia@gmail.com>
2025-05-14 07:30:39 -0300

    add componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <header>
+          <h1>Calcule seu IMC</h1>
+        </header>
+        <form>
+          <label> Digite sua altura</label>
+          <input type="number" placeholder="em centimetros" />
+          <label>Digite seu peso</label>
+          <input type="number" placeholder="em kg" />
+        </form>
+        <div>
+          <h3>O seu IMC é: 20</h3>
+          <p>Sua classificação é: Obeso nivel I</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+
   )
+
 }
 
 export default App
